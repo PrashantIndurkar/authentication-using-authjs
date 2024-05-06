b8695dbf766c43bec58d59544ed3f84ab3a5c3ab
prashantindurkar <prashantri2000@gmail.com>
2024-05-06 15:47:15 +0530

    feat: register user and store to db

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
 import * as z from "zod";
 
 // validate the action using zod schema
@@ -10,6 +13,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validateFields.success) {
     return { error: "Invalid Fields!" };
   }
+
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send veri token email
 
   return { success: "Email send!" };
 };
